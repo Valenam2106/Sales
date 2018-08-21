cf0bb5075dc4294ad769a2f04d6dab240b9497c0
Valentina Aguirre Muñoz <valenam2106@gmail.com>
2018-08-20 19:44:31 -0500

    Connection internet
    Connection internet

--- Sales/Sales/Services/ApiService.cs
+++ Sales/Sales/Services/ApiService.cs
@@ -6,9 +6,36 @@
     using System.Net.Http;
     using Newtonsoft.Json;
     using System.Collections.Generic;
+    using Plugin.Connectivity;
 
     public class ApiService
     {
+        public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings.",
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "No internet connection.",
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+            };
+        }
         //This method allows us to consume a list of an api service.
         public async Task<Response> GetList<T> (string urlBase, string prefix, string controller)
         {
--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -5,11 +5,14 @@
     using System.Collections.ObjectModel;
     using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
     using Services;
     using Xamarin.Forms;
     public class ProductsViewModel : BaseViewModel
     {
-        private ApiService apiService;  
+        private ApiService apiService;
+
+        private bool isRefreshing;
 
         private ObservableCollection<Product> products;
 
@@ -18,6 +21,13 @@
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
         }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
         public ProductsViewModel()
         {
             this.apiService = new ApiService();
@@ -25,15 +35,37 @@
         }
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
+
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+            }
+
             var url = Application.Current.Resources["UrlAPI"].ToString();
             var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
         }
     }
 }
